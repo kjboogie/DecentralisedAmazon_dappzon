dc2f5aa3a22cb97b42d8da24baa8cee2e53676b0
kjboogie <trickxbooks@gmail.com>
2023-04-16 00:44:38 +0530

    Solidity Backend completed

--- src/App.js
+++ src/App.js
@@ -13,11 +13,25 @@ import Dappazon from './abis/Dappazon.json'
 import config from './config.json'
 
 function App() {
+  const [account, setAccount] = useState(null)
+
+
+  const loadBlockchainData = async() =>{
+
+  }
+
+  
+
+  useEffect(() =>{
+    loadBlockchainData()
+  },[])
+
 
   return (
     <div>
-
-      <h2>Welcome to Dappazon</h2>
+      <Navigation account ={account} setAccount = {setAccount}/>
+      <h2>Dappazon Best Sellers</h2>
+      
 
     </div>
   );
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,127 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
+// it converts big number like (1 ether) to wei
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe("Dappazon", () => {
+//Global variables
+const ID = 1
+const NAME = "shoes"
+const CATEGORY = "Clothing"
+const IMAGE = "IMAGE"
+const COST = tokens(1)
+const RATING = 4
+const STOCK = 5
 
+describe("Dappazon", () => {
+  let dappazon
+  let deployer, buyer
+      beforeEach(async () => {
+         //Setup Accounts
+         [deployer, buyer] = await ethers.getSigners()
+         
+        // Deploy contract
+         const Dappazon = await ethers.getContractFactory("Dappazon") //fetch copy of smart contract
+         dappazon = await Dappazon.deploy()   //Deploy that contract
+      })
+
+
+
+    describe("Deployment", () => {
+      it("Sets the owner", async () => {
+        expect(await dappazon.owner()).to.equal(deployer.address)
+      })
+    })
+
+    describe("Listing", () => {
+      let transaction
+    
+
+      beforeEach(async () => {
+       transaction = await dappazon.connect(deployer).list(ID, NAME,CATEGORY,IMAGE,COST,RATING,STOCK) //calling the list function inside contract
+        await transaction.wait() //letting transation to get finished
+     })
+      it("Returns item attributes", async () => {
+        const item = await dappazon.items(ID)
+        //console.log(item)
+        expect(item.id).to.equal(ID)
+        expect(item.name).to.equal(NAME)
+        expect(item.category).to.equal(CATEGORY)
+        expect(item.image).to.equal(IMAGE)
+        expect(item.cost).to.equal(COST )
+        expect(item.id).to.equal(ID)
+        expect(item.id).to.equal(ID)
+      })
+
+      it("Emits List events", async () => {
+       expect(transaction).to.emit(dappazon, "List") 
+      })
+    })
+
+    describe("Buying", () => {
+      let transaction
+    
+
+      beforeEach(async () => {
+        // Listing Items
+        transaction = await dappazon.connect(deployer).list(ID, NAME,CATEGORY,IMAGE,COST,RATING,STOCK) //calling the list function inside contract
+        await transaction.wait() //letting transation to get finished
+        
+        // Buy an item
+        transaction = await dappazon.connect(buyer).buy(ID, { value: COST})
+      })
+     
+      it("Upgrade  buyer's order count", async () => {
+        const result = await dappazon.orderCount(buyer.address)
+        expect(result).to.equal(1)
+      })
+      it("Adds the order", async () =>{
+        const order = await dappazon.orders(buyer.address, 1)
+        expect(order.time).to.be.greaterThan(0)
+        expect(order.item.name).to.eq(NAME)
+      })
+      // it("Checks the byer is sending enough mone to buy",async () =>{
+      //    (await dappazon.connect(buyer).buy(ID, { value:  tokens(0.5)})).should.be.rejectedWith("VM Exception while processing transaction: reverted with reason string 'Please send the reuired cost to buy'")
+        
+      // })
+      it("Upgrade contract balance", async () => {
+        const result = await ethers.provider.getBalance(dappazon.address)
+        expect(result).to.equal(COST)
+      })
+      it("Emits a buy event", async() =>{
+        expect(transaction).to.emit(dappazon, "Buy")
+      })
+
+    })
+
+    describe("Withdrawl",async() => {
+      beforeEach(async () => {
+      //List an item
+      let transaction = await dappazon.connect(deployer).list(ID,NAME,CATEGORY,IMAGE,COST,RATING,STOCK)
+      transaction.wait()
+
+      // But an item
+      transcation = await dappazon.connect(buyer).buy(ID, {value: COST})
+      transaction.wait()
+      //Get deployer balance before
+      balanceBefore = await ethers.provider.getBalance(deployer.address)
+
+      //Withdrawn
+      transaction = await dappazon.connect(deployer).withdraw()
+      transaction.wait()
+    })
+    it('Updates the owner balance',async () =>{
+      const balanceAfter = await ethers.provider.getBalance(deployer.address)
+      expect(balanceAfter).to.be.greaterThan(balanceBefore)
+    })
+    it('Updates the contract balance',async() => {
+      const result = await ethers.provider.getBalance(dappazon.address)
+      expect(result).to.equal(0)
+    })
+    })
+    
+       
 })
+
